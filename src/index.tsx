a8a1a4d7643de4e2c4d39039725f712ec0d1a142
marcos <gianmotta7>
2021-10-28 13:45:49 -0300

    redux persist funionando

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,44 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, getDefaultMiddleware } from '@reduxjs/toolkit';
 import gameSlice from '../features/counter/gameSlice';
 import authSlice from '../features/counter/authSlice';
 import gameDetail from '../features/counter/gameDetailSlice';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import storageSession from 'redux-persist/lib/storage/session'
+
+import { combineReducers } from 'redux';
+
+const persistConfig = {
+  key: 'root',
+  storage: storage || storageSession,
+  whiteList: [], // Si no lo definimos guarda todo
+  blackList: []
+ };
+ 
+const rootReducer = combineReducers({
+  counter: gameSlice,
+  auth: authSlice,
+  gameDetail: gameDetail
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    counter: gameSlice,
-    auth: authSlice,
-    gameDetail: gameDetail
-  },
+  reducer:persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
-
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
@@ -19,3 +47,4 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+export const persistor = persistStore(store);
